fc1e60d48a216940ceff98de28ceccd8c98560fb
Pan Metrampaz <panmetrampaz@gmail.com>
2012-04-24 21:23:08 +0200

    slight change

--- aliasy.rb
+++ aliasy.rb
@@ -12,11 +12,25 @@ class MyApp < Sinatra::Base
     end
 end
 
-DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/urls.db") #there was my mistake! colon instead of dot.
+
+def random_str(size = 3)
+    charset = %w{ 2 3 4 6 7 9 A C D E F G H J K M N P Q R T V W X Y Z}
+    effect = (0...size).map{ charset.to_a[rand(charset.size)] }.join
+    if effect == Url.first(:u_id => effect) #check if there is the same unique key in database 
+        size << 1
+        random_str(size) 
+    else
+        puts effect
+    end 
+end
+
+
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/urls.db") 
 
 class Url
     include DataMapper::Resource
     property :id, Serial
+    property :u_id, String, :unique => true
     property :address, String, :required => true, :format => :url, :messages => {
         :format => "Not an URL",
         :presence => "Can't be empty unless you bribe me."
@@ -31,12 +45,13 @@ DataMapper.finalize
 DataMapper.auto_upgrade!
 
 
+
 get '/' do 
     erb :index
 end
 
 post '/' do
-	@link = Url.new(params) 
+	@link = Url.new(params, :u_id => random_str) 
     @link.save
     @message = ""
     if @link.save 
@@ -49,9 +64,9 @@ post '/' do
     erb @message, :layout => !request.xhr? 
 end
 
+
 get '/:suggestion' do 
-	query = params[:suggestion]
-	redirection = Url.first(:suggestion => query)
+	redirection = Url.first(:suggestion => params[:suggestion])
     if redirection
 	   redirect redirection[:address]
     else
