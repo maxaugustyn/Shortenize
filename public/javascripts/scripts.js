b92ad1ab184e6a48e9dd9511248d7cdb9f87a456
Pan Metrampaz <panmetrampaz@gmail.com>
2012-05-01 20:34:09 +0200

    Finally working javascript - can't imagine how stupid I was, attaching event to the handler that didn't exist at document.ready

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,28 +1,26 @@
-
 $(function() {
-    $("#submission").click(function(event){
-        event.preventDefault(); // self-explanatory
-        var address = $("input#address").val();
-        var suggestion = $("input#suggestion").val();
-        var postData = 'address='+address+'&suggestion='+suggestion;
-        $.ajax({
-          url: '/',
-          type: 'POST',
-          dataType: 'html',
-          data: postData,
-          success: function(data){
-            $(data).appendTo("#pane").hide().fadeIn(300);
-            console.log("Ajax successful!")
-          }
-        });    
-    });
-});
-
-$(function() {
-    $(".alert-box").on("click", "a.close", function(event) {
+    $(document).on("click", "a.close", function(event) {
         event.preventDefault();
-        $(this).closest(".alert-box").fadeOut(function(event) {
+        console.log("Suck My Dick");
+        $(event.target).closest(".alert-box").fadeOut(function() {
             $(this).remove();
         });
     });
-})
+
+    $("#submission").click(function(event) {
+        event.preventDefault(); // self-explanatory
+        var address = $("input#address").val();
+        var suggestion = $("input#suggestion").val();
+        var postData = 'address=' + address + '&suggestion=' + suggestion;
+        $.ajax({
+            url: '/',
+            type: 'POST',
+            dataType: 'html',
+            data: postData,
+            success: function(data) {
+                $(data).appendTo("#pane").hide().fadeIn(300);
+                console.log("Ajax successful!");
+            }
+        });
+    });
+});
